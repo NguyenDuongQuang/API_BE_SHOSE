0baca184e3377cf9b99a3468cb2f56fad433bd7a
NguyenDuongQuang <ndq1234pn@gmail.com>
2024-06-05 16:36:35 +0700

    build

---
+++ src/main/java/com/example/be_api_web/BeApiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_api_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeApiWebApplication.class, args);
+    }
+
+}
